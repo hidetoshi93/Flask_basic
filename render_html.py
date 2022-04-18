f9941abdc1280ac1045f63bd19ae7397d908fa67
hidetoshi <phunganhtuan.nd.vn@gmail.com>
2022-04-18 16:01:28 +0900

    update render html

---
+++ render_html.py
@@ -0,0 +1,23 @@
+from unittest.result import failfast
+from flask import Flask, redirect, url_for, render_template
+
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/user/admin')
+def hello_admin():
+    return "<h1> Hello admin!</h1>"
+
+@app.route('/user/<name>')
+def  hello_user(name):
+    if name == 'admin':
+        # redirect(hello_admin())
+        redirect(url_for('hello_admin'))
+
+    return f"<h1> Hello {name}!</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
