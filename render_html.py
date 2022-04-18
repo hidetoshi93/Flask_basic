d237d7d0b7463fa46d09ff0bbb7a43f53ce85ec0
hidetoshi <phunganhtuan.nd.vn@gmail.com>
2022-04-18 17:03:39 +0900

    update render template

--- render_html.py
+++ render_html.py
@@ -1,8 +1,8 @@
-from unittest.result import failfast
 from flask import Flask, redirect, url_for, render_template
 
 
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
